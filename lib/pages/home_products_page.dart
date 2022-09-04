12ec12b21c230c850b3a5d52f05cfcd8b27d4c78
David Onofre <davidonofre1010@gmail.com>
2022-09-04 14:35:44 -0500

    filtros con funcionalidad

--- lib/pages/home_products_page.dart
+++ lib/pages/home_products_page.dart
@@ -16,6 +16,8 @@ class _HomeProductsState extends State<HomeProducts> {
       body: SingleChildScrollView(
           child: Column(children: [
         _sectionGrey(),
+        _title(),
+        _rowFilters(),
       ])));
 
   AppBar _appBar() => AppBar(
@@ -109,4 +111,162 @@ class _HomeProductsState extends State<HomeProducts> {
         const Icon(Icons.keyboard_arrow_down,
             size: 30, color: AppColors.appTextLightColor)
       ]);
+
+  Container _title() => Container(
+      margin: const EdgeInsets.only(top: 20, left: 10, bottom: 15),
+      alignment: Alignment.centerLeft,
+      child: const Text('Snacks',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25)));
+
+  Row _rowFilters() => Row(children: [
+        const SizedBox(width: 10),
+        Expanded(
+            child: Card(
+                margin: EdgeInsets.zero,
+                color: AppColors.appBarColor,
+                child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text('Ordenar'),
+                          SizedBox(width: 10),
+                          Icon(Icons.swap_vert,
+                              color: AppColors.appIconColor, size: 30)
+                        ])))),
+        const SizedBox(width: 10),
+        Expanded(
+            child: GestureDetector(
+                onTap: _marcaOnTap,
+                child: Card(
+                    margin: EdgeInsets.zero,
+                    color: AppColors.appBarColor,
+                    child: Container(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Stack(children: <Widget>[
+                                Container(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 10),
+                                    child: const Text('Marcas')),
+                                Positioned(
+                                    top: 0,
+                                    right: 0,
+                                    child: Container(
+                                        width: 10,
+                                        height: 10,
+                                        decoration: BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            color: AppColors.appGreenColor,
+                                            border: Border.all(
+                                                color: Colors.white)),
+                                        child: Container()))
+                              ]),
+                              const Icon(Icons.keyboard_arrow_down,
+                                  color: AppColors.appIconColor, size: 30)
+                            ]))))),
+        const SizedBox(width: 10)
+      ]);
+
+  void _marcaOnTap() {
+    Map<String, bool> values = {
+      'lacanto': true,
+      'Marca comercial': false,
+      'lacantos': true,
+      'Marca comercials': true,
+      'lacantoa': true,
+      'Marca comerciala': false,
+    };
+    showModalBottomSheet(
+        context: context,
+        isScrollControlled: true,
+        isDismissible: true,
+        backgroundColor: Colors.transparent,
+        builder: (_) => Container(
+            height: MediaQuery.of(context).size.height / 1.9,
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(top: Radius.circular(25))),
+            child: SafeArea(
+                child: Column(children: [
+              GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                      alignment: Alignment.centerRight,
+                      margin: const EdgeInsets.only(right: 13, top: 13),
+                      child: const CircleAvatar(
+                          backgroundColor: AppColors.appTextLightColor,
+                          maxRadius: 12,
+                          child: Icon(Icons.close,
+                              size: 15, color: Colors.white)))),
+              Container(
+                  alignment: Alignment.centerLeft,
+                  margin: const EdgeInsets.only(top: 10, left: 15),
+                  child: const Text('Marcas',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w800, fontSize: 20))),
+              Flexible(
+                  child: StatefulBuilder(
+                      builder: (context, set) => ListView(
+                          padding: const EdgeInsets.only(top: 30),
+                          itemExtent: 40,
+                          children: values.keys
+                              .map((key) => CheckboxListTile(
+                                  title: Text(key,
+                                      style: const TextStyle(
+                                          color: AppColors.appTextLightColor)),
+                                  value: values[key],
+                                  activeColor: AppColors.appGreenColor,
+                                  checkboxShape: RoundedRectangleBorder(
+                                      side: const BorderSide(
+                                          color: AppColors.appTextLightColor),
+                                      borderRadius: BorderRadius.circular(5)),
+                                  onChanged: (value) {
+                                    set(() {
+                                      values[key] = value!;
+                                    });
+                                  }))
+                              .toList()))),
+              const Divider(color: AppColors.appTextLightColor),
+              _button('Filtrar (74 productos)')
+            ]))));
+  }
+
+  Container _button(String text, {bool isCounter = false}) => Container(
+      width: double.infinity,
+      margin: const EdgeInsets.symmetric(horizontal: 20),
+      child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+              primary: AppColors.appGreenColor,
+              padding: const EdgeInsets.symmetric(vertical: 5),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0)),
+              textStyle:
+                  const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+          child: isCounter
+              ? Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                      Container(
+                          margin: const EdgeInsets.only(left: 3),
+                          child: const CircleAvatar(
+                              backgroundColor: Colors.white,
+                              maxRadius: 15,
+                              child: Icon(Icons.add,
+                                  color: AppColors.appGreenColor))),
+                      const Text('0', style: TextStyle(fontSize: 16)),
+                      Container(
+                          margin: const EdgeInsets.only(right: 3),
+                          child: const CircleAvatar(
+                              backgroundColor: Colors.white,
+                              maxRadius: 15,
+                              child: Icon(Icons.remove,
+                                  color: AppColors.appGreenColor)))
+                    ])
+              : Text(text)));
 }
