34874858d128df0bea57d3682a340b26c1ea48ca
David Onofre <davidonofre1010@gmail.com>
2022-09-04 13:31:58 -0500

    colores app

---
+++ lib/resources/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const appBarColor = Color(0xFFF4F4F4);
+  static const appGreenColor = Color(0xFF27884D);
+  static const appIconColor = Color(0xFF2D2D2D);
+  static const appTextColor = Color(0xFF000000);
+  static const appTextLightColor = Color(0xFF676769);
+}
